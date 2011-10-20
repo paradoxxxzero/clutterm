2485b928ee6f420ca0f9f5a95a60e4d25c9f9d79
Florian Mounier <florian.mounier@kozea.fr>
2011-10-20 12:21:35 +0200

    Handle colors correctly

--- clutterm/lex.py
+++ clutterm/lex.py
@@ -1,47 +1,40 @@
 import logging
 import re
 log = logging.getLogger('clutterm')
-color = {
-    30: '#262524',
-    31: '#bf4646',
-    32: '#67b25f',
-    33: '#cfc44e',
-    34: '#516083',
-    35: '#ca6eff',
-    36: '#92b2f8',
-    37: '#d5d5d5',
-    40: '#292827',
-    41: '#f48a8a',
-    42: '#a5d79f',
-    43: '#e1da84',
-    44: '#a2bbff',
-    45: '#e2b0ff',
-    46: '#bacdf8',
-    47: '#ffffff'
+color = (
+    '#262524',
+    '#bf4646',
+    '#67b25f',
+    '#cfc44e',
+    '#516083',
+    '#ca6eff',
+    '#92b2f8',
+    '#d5d5d5'
+)
+
+bold_color = (
+    '#292827',
+    '#f48a8a',
+    '#a5d79f',
+    '#e1da84',
+    '#a2bbff',
+    '#e2b0ff',
+    '#bacdf8',
+    '#ffffff'
+)
+
+escape_chars = {
+    '<': '&lt;',
+    '>': '&gt;',
+    '&': '&amp;'
 }
 
-bold_color = {
-    30: '#292827',
-    31: '#f48a8a',
-    32: '#a5d79f',
-    33: '#e1da84',
-    34: '#a2bbff',
-    35: '#e2b0ff',
-    36: '#bacdf8',
-    37: '#ffffff'
-}
+csi_re = re.compile(r'\x1b\[(\?)?(\d*)(;(\d*))?([a-zA-z@])')
+osc_re = re.compile(r'\x1b\](\d+);(.*)\x07')
 
 
 class Lexer(object):
     """Mayhem parser"""
-
-    escape_chars = {
-        '<': '&lt;',
-        '>': '&gt;',
-        '&': '&amp;'
-    }
-    csi_re = re.compile(r'\x1b\[(\?)?(\d*)(;(\d*))?([a-zA-z@])')
-    osc_re = re.compile(r'\x1b\](\d+);(.*)\x07')
 
     def __init__(self, text, cursor, string, set_title):
         self.text = text
@@ -57,12 +50,20 @@ class Lexer(object):
         if type == 'm':
             if n < 30 or n in (39, 49):
                 style = '</span><span>'
-            elif m != 1:
-                style = ('</span><span foreground="%s">' %
-                        bold_color[n])
-            else:
-                style = ('</span><span foreground="%s">' %
-                        color[n])
+            elif 30 <= n <= 37:
+                if m != 1:
+                    style = ('</span><span foreground="%s">' %
+                             color[n - 30])
+                else:
+                    style = ('</span><span foreground="%s">' %
+                             bold_color[n - 30])
+            elif 40 <= n <= 47:
+                if m != 1:
+                    style = ('</span><span background="%s">' %
+                             color[n - 40])
+                else:
+                    style = ('</span><span background="%s">' %
+                             bold_color[n - 40])
             # FIXME
             if self.cursor > 0 and self.cursor <= len(self.string):
                 self.string[self.cursor - 1] += style
@@ -96,13 +97,14 @@ class Lexer(object):
 
         while self.position != len(self.text):
             char = self.text[self.position]
-            csi = self.csi_re.match(self.text[self.position:])
-            osc = self.osc_re.match(self.text[self.position:])
+            csi = csi_re.match(self.text[self.position:])
+            osc = osc_re.match(self.text[self.position:])
 
             self.position += 1
+
             # Pango escaping
-            if char in self.escape_chars:
-                char = self.escape_chars[char]
+            if char in escape_chars:
+                char = escape_chars[char]
 
             elif csi:
                 self.csi(csi)
@@ -112,10 +114,8 @@ class Lexer(object):
                 self.osc(osc)
                 continue
             elif char == '\x1b':
-                import pdb
-                pdb.set_trace()
-                log.error('Unmatched escape %d %r' % (
-                    self.position, self.text))
+                log.error('Unmatched escape %r' % (
+                    self.text[self.position:][:10]))
                 continue
 
             elif char == '\r':
