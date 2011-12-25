badb0328ac563dde1a867418ecc8c81cd57e8da8
Florian Mounier <florian.mounier@kozea.fr>
2011-12-25 23:28:55 +0100

    Remove useless layouts

--- clutterm/ui.py
+++ clutterm/ui.py
@@ -21,7 +21,8 @@ class Clutterm(object):
         """
         self.itime = time()
         self.shader = None
-        self.font = "Mono 10"
+        self.font = "Mono 12"
+        self.size = None, None
         self.mainStage = Clutter.Stage.new()
         self.mainStage.set_title("Clutterminal")
         self.mainStage.set_reactive(True)
@@ -29,20 +30,12 @@ class Clutterm(object):
         self.mainStage.set_use_alpha(True)
         self.mainStage.set_color(colorBlack)
 
-        # Create lines layout
-        self.linesBoxManager = Clutter.BoxLayout()
-        self.linesBoxManager.set_vertical(True)
-        self.linesBoxManager.set_homogeneous(False)
-        self.linesBoxManager.set_pack_start(False)
-        # self.linesBoxManager.set_use_animations(True)
-        # self.linesBoxManager.set_easing_duration(250)
-
-        # Create the lines box
-        self.linesBox = Clutter.Box.new(self.linesBoxManager)
-        self.mainStage.add_actor(self.linesBox)
+        # Create lines group
+        self.linesGroup = Clutter.Group()
+        self.mainStage.add_actor(self.linesGroup)
         dummy_text = Clutter.Text()
         dummy_text.set_font_name(self.font)
-        dummy_text.set_text("m")
+        dummy_text.set_text("&")
         self.char_width = dummy_text.get_width()
         self.char_height = dummy_text.get_height()
 
@@ -56,41 +49,43 @@ class Clutterm(object):
         self.cursor.set_y(self.char_height * self.lexer.cursor.y)
         self.cursor.set_width(self.char_width)
         self.cursor.set_height(self.char_height)
-        self.mainStage.add_actor(self.cursor)
+        self.linesGroup.add_actor(self.cursor)
 
-        def create_line():
+        def create_line(i):
             line = Clutter.Text()
             line.set_color(colorWhite)
             line.set_cursor_color(colorRed)
             line.set_selected_text_color(colorRed)
             line.set_font_name(self.font)
-            # self.line.set_editable(True)
             line.set_selectable(True)
             line.set_cursor_visible(True)
-            self.linesBoxManager.set_alignment(line, 0, 0)
-            self.linesBox.add_actor(line)
+            line.set_y(i * self.char_height)
+            self.linesGroup.add_actor(line)
             return line
 
-        def resize(a0, a1):
+        def resize(w, h):
             w = self.mainStage.get_width()
             h = self.mainStage.get_height()
             cols = int(w / self.char_width)
             rows = int(h / self.char_height)
+            if (cols, rows) == (self.lexer.cols, self.shell.rows):
+                return
             log.info('resize %s %s %s %s' % (w, h, cols, rows))
             self.shell.resize(cols, rows)
             self.lexer.resize(cols, rows)
-            self.linesBox.set_geometry(self.mainStage.get_geometry())
-            if rows > len(self.lines):
-                for i in range(rows - len(self.lines)):
-                    self.lines.append(create_line())
+            self.linesGroup.set_geometry(self.mainStage.get_geometry())
+            for line in self.lines:
+                self.linesGroup.remove_actor(line)
+            self.lines = [create_line(i)
+                          for i in range(self.shell.rows)]
 
-        self.lines = [create_line()
+        self.lines = [create_line(i)
                       for i in range(self.shell.rows)]
 
         self.thread = ReaderAsync(self.shell, self.write)
         self.thread.start()
 
-        Clutter.threads_add_timeout(1, 40, self.tick, None)
+        # Clutter.threads_add_timeout(300, 40, self.tick, None)
         # Setup key bindings on the terminal
         self.mainStage.connect_after("key-press-event", self.onKeyPress)
         self.mainStage.connect_after("notify::width", resize)
@@ -98,7 +93,7 @@ class Clutterm(object):
         self.mainStage.set_size(
             self.shell.cols * self.char_width,
             self.shell.rows * self.char_height)
-
+        self.linesGroup.set_geometry(self.mainStage.get_geometry())
         # Present the main stage (and make sure everything is shown)
         self.mainStage.show_all()
 
@@ -110,9 +105,7 @@ class Clutterm(object):
         for line in self.lexer.damaged:
             self.set_line(line, self.lexer.get_line(line))
         self.lexer.damaged = set()
-        # color = Clutter.Color()
-        # color.from_string(self.lexer.current_fg)
-        # self.cursor.set_color(color)
+
         self.cursor.animatev(
             Clutter.AnimationMode.LINEAR, 50,
             (
@@ -128,7 +121,7 @@ class Clutterm(object):
         self.mainStage.set_title(text)
 
     def bell(self):
-        self.linesBox.animatev(
+        self.linesGroup.animatev(
             Clutter.AnimationMode.EASE_OUT_BACK, 100,
             (
                 "fixed::scale-x",
@@ -140,8 +133,8 @@ class Clutterm(object):
             ), (
                 1.2,
                 1.2,
-                self.linesBox.get_width() / 2,
-                self.linesBox.get_height() / 2,
+                self.linesGroup.get_width() / 2,
+                self.linesGroup.get_height() / 2,
                 1,
                 1
             )
@@ -188,14 +181,14 @@ class Clutterm(object):
 
         if kval in shaders:
             self.shader = None
-            shaders[kval](self.linesBox)
+            shaders[kval](self.linesGroup)
             return
 
         if kval == 65475:
             self.shader = apply_glsl_effect(
-                self.linesBox,
-                self.mainStage.get_width(),
-                self.mainStage.get_height())
+                self.linesGroup,
+                self.linesGroup.get_width(),
+                self.linesGroup.get_height())
             return
 
         elif kval == 65480:
